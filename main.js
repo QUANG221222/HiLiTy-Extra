d2f4c10f0e9ffd7f512dd83bcd9deb30c53183c8
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-08-20 16:15:54 +0700

    Update Full Source Code Web HiLiTy-Extra

---
+++ main.js
@@ -0,0 +1,33 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 50000000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    // dots
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".coming-container", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+});
