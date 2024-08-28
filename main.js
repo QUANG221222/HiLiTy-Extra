5d92edc6c4ea27fd80dc64bd9865b956ad506a3a
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-08-28 18:33:48 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 let header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
-
+let navbar_a = document.querySelectorAll(".navbar li a");
+let sections = document.querySelectorAll("section");
+// ==========================
+// Navbar
+// window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+//     if (top >= offset && top < offset + height) {
+//       navbar_a.forEach((links) => {
+//         links.classList.remove("active");
+//         document
+//           .querySelector(".navbar a[href*=" + id + "]")
+//           .classList.add("active");
+//       });
+//     }
+//   });
+// };
+navbar_a.forEach((element) => {
+  element.addEventListener("click", () => {
+    removeActive();
+    element.classList.add("active");
+  });
+});
+function removeActive() {
+  for (let el of navbar_a) {
+    el.classList.remove("active");
+  }
+}
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
 });
