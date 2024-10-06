415610ce3ed9b7afbf47416e42cceffb2eaf24c1
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-10-06 15:23:36 +0700

    update

--- main.js
+++ main.js
@@ -3,39 +3,9 @@ let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 let navbar_a = document.querySelectorAll(".navbar li a");
 let sections = document.querySelectorAll("section");
+console.log(sections[0].offsetTop);
+console.log(navbar_a);
 // ==========================
-// Navbar
-// window.onscroll = () => {
-//   sections.forEach((sec) => {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 150;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute("id");
-//     if (top >= offset && top < offset + height) {
-//       navbar_a.forEach((links) => {
-//         links.classList.remove("active");
-//         document
-//           .querySelector(".navbar a[href*=" + id + "]")
-//           .classList.add("active");
-//       });
-//     }
-//   });
-// };
-navbar_a.forEach((element) => {
-  element.addEventListener("click", () => {
-    removeActive();
-    element.classList.add("active");
-  });
-});
-function removeActive() {
-  for (let el of navbar_a) {
-    el.classList.remove("active");
-  }
-}
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
-});
-
 menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("active");
@@ -44,6 +14,38 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+// Navbar
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    // let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset) {
+      navbar_a.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector(".navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+// navbar_a.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     removeActive();
+//     element.classList.add("active");
+//   });
+// });
+// function removeActive() {
+//   for (let el of navbar_a) {
+//     el.classList.remove("active");
+//   }
+// }
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
 var swiper = new Swiper(".home", {
   spaceBetween: 30,
   centeredSlides: true,
